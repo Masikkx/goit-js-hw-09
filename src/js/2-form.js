cc3e56b9426518f9e625de9226d7dae3f9bdd90e
Maksym <www.macs2009@gmail.com>
2025-10-02 13:14:55 +0300

    Do all tasks

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,33 @@
-console.log("form");
+let formData = {
+  email: "",
+  message: ""
+};
+const form = document.querySelector(".feedback-form");
+const savedData = localStorage.getItem("feedback-form-state");
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || "";
+  form.elements.message.value = formData.message || "";
+}
+
+form.addEventListener("input", event => {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem("feedback-form-state");
+  formData = { email: "", message: "" };
+  form.reset();
+});
